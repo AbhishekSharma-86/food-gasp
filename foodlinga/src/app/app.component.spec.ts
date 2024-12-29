05d843f162ab1044ceff0927186de12f99cc26fa
Muskaan Bhardwaj <muskaanbhardwaj13@gmail.com>
2024-12-29 23:12:25 +0530

    initial html , css code

--- foodlinga/src/app/app.component.spec.ts
+++ foodlinga/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'foodlinga' title`, () => {
+  it(`should have the 'foodinga' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('foodlinga');
+    expect(app.title).toEqual('foodinga');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, foodlinga');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, foodinga');
   });
 });
---
+++ foodlinga/src/app/contact-us/contact-us.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from "../header/header.component";
+
+@Component({
+  selector: 'app-contact-us',
+  imports: [HeaderComponent],
+  templateUrl: './contact-us.component.html',
+  styleUrl: './contact-us.component.scss'
+})
+export class ContactUsComponent {
+
+}
